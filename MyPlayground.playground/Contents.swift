e93be4fdda0aa5230b63d15dc804476493463d3f
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-12-03 10:43:19 -0500

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+// How to print
+print("Hello World")
+
+// Create a variable
+var myvariable = 44
+myvariable = 55
+print(myvariable)
+
+// Create a constant
+let constant = 33
+print(constant)
+
+// Types 
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+print(explicitDouble)
+let explicitFloat: Float = 88
+print(explicitFloat)
+
+// Type Conversion
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
