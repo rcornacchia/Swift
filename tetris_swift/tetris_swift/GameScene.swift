16eaecd7290e6c5f09d1770009f9939d5537f9c0
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-12-04 16:52:36 -0500

    Started work on swiftris

---
+++ tetris_swift/tetris_swift/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  tetris_swift
+//
+//  Created by Rob Cornacchia on 12/4/15.
+//  Copyright (c) 2015 Blackbird. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ tetris_swift/tetris_swift/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  tetris_swift
+//
+//  Created by Rob Cornacchia on 12/4/15.
+//  Copyright (c) 2015 Blackbird. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
